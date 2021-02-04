5236f919771c5d5d04fa86e99baedfdafa211660
Matthew Simpson <mjsimpson@coastal.edu>
2021-02-03 19:13:39 -0500

    Created Rectangle Class

---
+++ src/Rectangle.php
@@ -0,0 +1,9 @@
+<?php
+class Rectangle extends Shape{
+    const SHAPE_TYPE = 2;
+
+    public function getFullDescription($id, $name, $length, $width)
+    {
+        echo "Rectangle<$id>: $name - $length x $width";
+    }
+    }
